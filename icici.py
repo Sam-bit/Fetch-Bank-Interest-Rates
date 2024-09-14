0a3935f4af99e48676e4403b740bbb9283bdf8e7
Sambit Samal <itsme.sambit1991@gmail.com>
2024-09-14 08:24:14 +0530

    added - ICICI SB

--- icici.py
+++ icici.py
@@ -6,9 +6,9 @@ from bs4 import BeautifulSoup
 import re, os
 import datetime
 
-
-refresh = True
+refresh = False
 headers = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0'}
+
 
 def parse_tenure(tenureText=None):
     print(tenureText)
@@ -19,12 +19,12 @@ def parse_tenure(tenureText=None):
         return a, 'D', 'GE', b, 'D', 'LE'
     B = re.search(r'(\d+) Days to < (\d+) Year', tenureText)
     if B:
-        a =B.group(1)
+        a = B.group(1)
         b = B.group(2)
         return a, 'D', 'GE', b, 'Y', 'LT'
-    C =  re.search(r'(\d+) Year to < (\d+) Months', tenureText)
+    C = re.search(r'(\d+) Year to < (\d+) Months', tenureText)
     if C:
-        a =C.group(1)
+        a = C.group(1)
         b = C.group(2)
         return a, 'Y', 'GE', b, 'M', 'LT'
     D = re.search(r'(\d+) Months to < (\d+) Months', tenureText)
@@ -70,14 +70,21 @@ def parse_tenure(tenureText=None):
         a = L.group(1)
         b = L.group(2)
         return a, 'Y', 'GE', b, 'Y', 'LT'
+
+
 def get_roi(text_value):
     return float((text_value.split(" (")[0] if "(" in text_value else text_value).replace('\u200b', ''))
 
+
 def removeComments(string):
-    string = re.sub(re.compile("/\*.*?\*/",re.DOTALL ) ,"" ,string) # remove all occurrences streamed comments (/*COMMENT */) from string
-    string = re.sub(re.compile("//.*?\n" ) ,"" ,string) # remove all occurrence single-line comments (//COMMENT\n ) from string
-    string = re.sub(r'(\w+):', r'"\1":', string) # Fix unquoted keys (convert them to quoted keys)
+    string = re.sub(re.compile("/\*.*?\*/", re.DOTALL), "",
+                    string)  # remove all occurrences streamed comments (/*COMMENT */) from string
+    string = re.sub(re.compile("//.*?\n"), "",
+                    string)  # remove all occurrence single-line comments (//COMMENT\n ) from string
+    string = re.sub(r'(\w+):', r'"\1":', string)  # Fix unquoted keys (convert them to quoted keys)
     return string
+
+
 def appendtocsv(row=None):
     if row is None:
         row = []
@@ -90,7 +97,9 @@ def appendtocsv(row=None):
             file.write(','.join([str(x).replace('\u200b', '') for x in row]))
             file.write('\n')
 
+
 import ast
+
 _BANK_NAME = 'ICICI'
 if refresh:
     open("rates_list.csv", 'w').close()
@@ -100,17 +109,19 @@ header = ['bank_name', 'old_or_new', 'deposit', 'deposit_name', 'from_year', 'fr
           'general_roi', 'senior_roi']
 appendtocsv(header)
 
-_senior_rate_inc =0
-_BANK_FD_URL ='https://www.icicibank.com/personal-banking/deposits/fixed-deposit/fd-interest-rates'
-r = requests.get(_BANK_FD_URL,headers=headers)
+_senior_rate_inc = 0
+_BANK_FD_URL = 'https://www.icicibank.com/personal-banking/deposits/fixed-deposit/fd-interest-rates'
+r = requests.get(_BANK_FD_URL, headers=headers)
 soup = BeautifulSoup(r.text.encode('utf-8'), "html.parser")
-effectDate = soup.find("div",class_ = "interest-rates").find('h2').text.replace('FD Interest Rate for General and Senior Citizens(w.e.f. from ','').replace(')','')
+effectDate = soup.find("div", class_="interest-rates").find('h2').text.replace(
+    'FD Interest Rate for General and Senior Citizens(w.e.f. from ', '').replace(')', '')
 from_year = datetime.datetime.strptime(effectDate, "%b %d, %Y").strftime("%d-%m-%Y")
 _INTEREST_FD_URL = 'https://www.icicibank.com/content/dam/icicibank/india/managed-assets/revamp-pages/fixed-deposits-all-pages/script/retail-new-data.js'
-r = requests.get(_INTEREST_FD_URL,headers=headers)
-filetext = removeComments(r.text).replace('\t','').replace('  ','').replace('\n','').replace('\r','').replace("'", '"')
-interestre = re.findall(r'interestData = .*];', filetext,flags=re.MULTILINE)
-interestData = interestre[0].replace('interestData = ','').replace(';','')
+r = requests.get(_INTEREST_FD_URL, headers=headers)
+filetext = removeComments(r.text).replace('\t', '').replace('  ', '').replace('\n', '').replace('\r', '').replace("'",
+                                                                                                                  '"')
+interestre = re.findall(r'interestData = .*];', filetext, flags=re.MULTILINE)
+interestData = interestre[0].replace('interestData = ', '').replace(';', '')
 numList = 1
 interestDataDict = None
 try:
@@ -136,40 +147,60 @@ if interestDataDict is not None:
             if numList in amount_ranges:
                 min_amount, max_amount = amount_ranges[numList]
             else:
-                min_amount,max_amount = 0,0
-            from_period, from_period_d_m_y,from_period_operator,to_period, to_period_d_m_y,to_period_operator = parse_tenure(values[0])
+                min_amount, max_amount = 0, 0
+            from_period, from_period_d_m_y, from_period_operator, to_period, to_period_d_m_y, to_period_operator = parse_tenure(
+                values[0])
             if max_amount == 30000000 and from_period == 15 and to_period == 18 and from_period_d_m_y == 'M' and to_period_d_m_y == 'M':
                 _senior_rate_inc = 0.55
-            each_row = [_BANK_NAME, 'NEW', 'FD', 'Regular Fixed Deposit', from_year, from_period, from_period_d_m_y, from_period_operator, '',
+            each_row = [_BANK_NAME, 'NEW', 'FD', 'Regular Fixed Deposit', from_year, from_period, from_period_d_m_y,
+                        from_period_operator, '',
                         to_period, to_period_d_m_y,
-                        to_period_operator, min_amount, max_amount, 'Y', values[1], float(values[1]) + float(_senior_rate_inc)]
+                        to_period_operator, min_amount, max_amount, 'Y', values[1],
+                        float(values[1]) + float(_senior_rate_inc)]
             appendtocsv(each_row)
-            each_row = [_BANK_NAME, 'NEW', 'FD', 'Regular Fixed Deposit', from_year, from_period, from_period_d_m_y, from_period_operator, '',
+            each_row = [_BANK_NAME, 'NEW', 'FD', 'Regular Fixed Deposit', from_year, from_period, from_period_d_m_y,
+                        from_period_operator, '',
                         to_period, to_period_d_m_y,
-                        to_period_operator, min_amount, max_amount, 'Y', values[2], float(values[2]) + float(_senior_rate_inc)]
+                        to_period_operator, min_amount, max_amount, 'Y', values[2],
+                        float(values[2]) + float(_senior_rate_inc)]
             appendtocsv(each_row)
-            each_row = [_BANK_NAME, 'NEW', 'FD', 'Regular Fixed Deposit', from_year, from_period, from_period_d_m_y, from_period_operator, '',
+            each_row = [_BANK_NAME, 'NEW', 'FD', 'Regular Fixed Deposit', from_year, from_period, from_period_d_m_y,
+                        from_period_operator, '',
                         to_period, to_period_d_m_y,
-                        to_period_operator, min_amount, max_amount, 'N', values[3], float(values[3]) + float(_senior_rate_inc)]
+                        to_period_operator, min_amount, max_amount, 'N', values[3],
+                        float(values[3]) + float(_senior_rate_inc)]
             appendtocsv(each_row)
-            each_row = [_BANK_NAME, 'NEW', 'FD', 'Regular Fixed Deposit', from_year, from_period, from_period_d_m_y, from_period_operator, '',
+            each_row = [_BANK_NAME, 'NEW', 'FD', 'Regular Fixed Deposit', from_year, from_period, from_period_d_m_y,
+                        from_period_operator, '',
                         to_period, to_period_d_m_y,
-                        to_period_operator, min_amount, max_amount, 'N', values[4], float(values[4]) + float(_senior_rate_inc)]
+                        to_period_operator, min_amount, max_amount, 'N', values[4],
+                        float(values[4]) + float(_senior_rate_inc)]
             appendtocsv(each_row)
-        numList+=1
+        numList += 1
 _BANK_RD_URL = 'https://www.icicibank.com/personal-banking/deposits/recurring-deposits/rd-interest-rate'
-r = requests.get(_BANK_RD_URL,headers=headers)
+r = requests.get(_BANK_RD_URL, headers=headers)
 soup = BeautifulSoup(r.text.encode('utf-8'), "html.parser")
-tablediv = soup.find('div',class_= 'accordion-content').find('table')
+tablediv = soup.find('div', class_='accordion-content').find('table')
 effectDate = tablediv.find('thead').find('tr').find_all('th')[1].find('b').text
 from_year = datetime.datetime.strptime(effectDate, "%d %B, %Y").strftime("%d-%m-%Y")
 all_trs = tablediv.find('tbody').find_all('tr')
 for tr in all_trs:
-    from_period, from_period_d_m_y,from_period_operator,to_period, to_period_d_m_y,to_period_operator = parse_tenure(tr.find_all('td')[0].text)
-    generalroi = tr.find_all('td')[1].text.replace("%","")
-    seniorroi = tr.find_all('td')[2].text.replace("%","")
+    from_period, from_period_d_m_y, from_period_operator, to_period, to_period_d_m_y, to_period_operator = parse_tenure(
+        tr.find_all('td')[0].text)
+    generalroi = tr.find_all('td')[1].text.replace("%", "")
+    seniorroi = tr.find_all('td')[2].text.replace("%", "")
     each_row = [_BANK_NAME, 'NEW', 'RD', 'Regular Recurring Deposit', from_year, from_period, from_period_d_m_y,
                 from_period_operator, '',
                 to_period, to_period_d_m_y,
                 to_period_operator, 500, 1000000, 'N', generalroi, seniorroi]
     appendtocsv(each_row)
+each_row = [_BANK_NAME, 'NEW', 'SB', 'Regular Saving Deposit', '04-06-2020', '', '',
+            '', '',
+            '', '',
+            '', 1, 5000000, '', 3, 3]
+appendtocsv(each_row)
+each_row = [_BANK_NAME, 'NEW', 'SB', 'Regular Saving Deposit', '04-06-2020', '', '',
+            '', '',
+            '', '',
+            '', 5000000, 99999999999, '', 3.5, 3.5]
+appendtocsv(each_row)
